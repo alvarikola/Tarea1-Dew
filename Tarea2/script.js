c047e742f81565d0e348b8630fa4a7cd242fba6b
Alvaro <alvaroe484@gmail.com>
2025-10-06 12:12:00 +0100

    avances dia 6/10

--- Tarea2/script.js
+++ Tarea2/script.js
@@ -9,32 +9,106 @@ function navegarPantallas(pantalla){
 
 
 
-//Declarar un objeto
-var Avion = function(rows, columns, compañia, price) {
+// Clase Avion
+function Avion(rows, columns, compañia, precioBase) {
     this.rows = rows;
     this.columns = columns;
     this.compañia = compañia;
-    this.price = price;
-    this.asiento = [];
-    document.write("<div class='avion'>");
-    for (let r = 0; r < rows; r++) {
-        document.write("<div class='filas'>");
-        this.asiento[r] = [];
-        for (let c = 0; c < columns; c++) {
-            document.write("<div class=''>" + c + "</div>")
-            this.asiento[r][c] = false;
+    this.precioBase = precioBase;
+    this.asientos = [];
+
+    const storedAsientos = localStorage.getItem(`asientos_${this.compañia}`);
+
+    if (storedAsientos) {
+        this.asientos = JSON.parse(storedAsientos);
+    } else {
+        // Inicializa matriz de asientos (todos libres)
+        for (let i = 0; i < rows; i++) {
+            this.asientos[i] = [];
+            for (let j = 0; j < columns; j++) {
+                this.asientos[i][j] = false; // false = libre
+            }
         }
-        document.write("</div>");
     }
-    document.write("</div>");
-    this.reservar =function() {
-        console.log("Buy Theatre ticket "+this.price+" €");
+
+    // Guardar asientos en localStorage
+    this.guardarEstado = function() {
+        localStorage.setItem(`asientos_${this.compañia}`, JSON.stringify(this.asientos));
     };
-    this.liberar =function() {
-        console.log("Buy Theatre ticket "+this.price+" €");
+
+    // Reservar asiento
+    this.reservarAsiento = function (fila, columna) {
+        if (!this.asientos[fila][columna]) {
+            this.asientos[fila][columna] = true;
+            this.guardarEstado();
+            return true;
+        } else {
+            alert("Asiento ya ocupado");
+            return false;
+        }
+    };
+
+    // Liberar asiento
+    this.liberarAsiento = function (fila, columna) {
+        if (this.asientos[fila][columna]) {
+            this.asientos[fila][columna] = false;
+            this.guardarEstado();
+            return true;
+        }
+        return false;
+    };
+
+    // Mostrar tabla de asientos
+    this.mostrarTabla = function () {
+        document.write(`<table class="asientos">`);
+        for (let i = 0; i < this.rows; i++) {
+            document.write("<tr>");
+            for (let j = 0; j < this.columns; j++) {
+                let clase = "lowcost";
+                if (i < 2) clase = "business";
+                else if (i < 5) clase = "economica";
+
+                let precioFinal = this.precioBase;
+                if (clase === "business") precioFinal *= 2;
+                else if (clase === "economica") precioFinal *= 1.5;
+
+                const ocupado = this.asientos[i][j];
+
+                // Convertimos fila a letra (A, B, C...) y columna a número (1, 2, 3...)
+                const letraFila = String.fromCharCode(65 + i); // A = 65
+                const numeroColumna = j + 1;
+                document.write(`
+                    <td onclick="toggleReservaAsiento(${i},${j})" class="${clase} ${ocupado ? 'ocupado' : 'libre'}">
+                        ${letraFila}${numeroColumna}
+                        <br>${precioFinal}€
+                    </td>`);
+            }
+            document.write("</tr>");
+        }
+        document.write("</table>");
     };
 }
 
-let avion1 = new Avion(4, 45, "binter", 10);
 
 
+
+
+
+let avion = new Avion(6, 45, "Binter", 10)
+avion.mostrarTabla();
+
+// Función global para reservar o liberar asiento con toggle
+function toggleReservaAsiento(fila, columna) {
+    if (!avion.asientos[fila][columna]) {
+        // Si está libre, reservar
+        if (avion.reservarAsiento(fila, columna)) {
+            alert(`Asiento ${String.fromCharCode(65 + fila)}${columna + 1} reservado correctamente.`);
+        }
+    } else {
+        // Si está ocupado, liberar
+        if (avion.liberarAsiento(fila, columna)) {
+            alert(`Asiento ${String.fromCharCode(65 + fila)}${columna + 1} liberado correctamente.`);
+        }
+    }
+    location.reload(); // Refresca para actualizar visualmente
+}
