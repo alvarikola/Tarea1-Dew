b521f75d486f2d80f204c9b491ce39a2108ae275
Alvaro <alvaroe484@gmail.com>
2025-11-10 12:09:40 +0000

    nueva tarea 10/11

---
+++ Tarea5/script.js
@@ -0,0 +1,13 @@
+const patterns = {
+  nombre: /^[A-Z][a-z\d]$/i,
+  apellidos: /^[A-Z][a-z]+(?:\s[A-Z][a-z]+)?$/i,
+  dni: /^[x]*\d{8}[a-z]$/i,
+  fechaNacimiento: /^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$/i,
+  codigoPostal: ,
+  email: ,
+  telefonoFijo: ,
+  telefonoMovil: ,
+  iban: ,
+  tarjetaCredito: ,
+  contrasena:
+};
