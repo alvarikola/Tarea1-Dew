83b9a48489108d180c58a1ef9fe0d5822632fb96
Alvaro <alvaroe484@gmail.com>
2025-11-11 10:04:48 +0000

    avances 11/11

--- Tarea5/script.js
+++ Tarea5/script.js
@@ -2,12 +2,12 @@ const patterns = {
   nombre: /^[A-Z][a-z\d]$/i,
   apellidos: /^[A-Z][a-z]+(?:\s[A-Z][a-z]+)?$/i,
   dni: /^[x]*\d{8}[a-z]$/i,
-  fechaNacimiento: /^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$/i,
-  codigoPostal: ,
-  email: ,
-  telefonoFijo: ,
-  telefonoMovil: ,
-  iban: ,
-  tarjetaCredito: ,
-  contrasena:
+  fechaNacimiento: /^([0-2][0-9]|3[0-1])(\/)(0[1-9]|1[0-2])\2(\d{4})$/i,
+  codigoPostal: /^([0-4][0-9]|5[0-2])(\d{3})$/i,
+  email: /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/i,
+  telefonoFijo: /^([89]\d{2}(\s?\d{2}){3})$/i,
+  telefonoMovil: /^([67]\d{2}(\s?\d{2}){3})$/i,
+  iban: /[a-zA-Z]{2}[0-9]{20}$/i,
+  tarjetaCredito: /^(\d{4}[\s]?){3}\d{4}$/i,
+  contrasena: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$?¡_\-])[A-Za-z\d@$?¡_\-]{12,}$/i
 };
